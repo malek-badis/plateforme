72042e42a5db84ab59687c24f0e30d2669c5c21d
badis malek <badismalek123@gmail.com>
2021-05-01 02:38:49 +0100

    Migration created for product and reviews

--- database/migrations/2021_05_01_002939_create_products_table.php
+++ database/migrations/2021_05_01_002939_create_products_table.php
@@ -14,7 +14,10 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->text('detail');
+            $table->integer('value');
             $table->timestamps();
         });
     }
