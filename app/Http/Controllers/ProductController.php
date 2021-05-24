d74807d00e56d17fc9b58b6b2c2f7cbd84d23193
badis malek <badismalek123@gmail.com>
2021-05-24 22:48:25 +0100

    product destroy

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -99,6 +99,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
-    }
+        $product->delete();
+        return response(null,Response::HTTP_CREATED);
+            }
 }
